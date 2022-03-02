77255cf02fc08938f2d926f8a8889c1eef5536fc
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2022-03-02 20:13:26 +0100

    fixed: img

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
+const allNav = document.querySelectorAll('nav__link')
+
 
 const handleNav = () => {
-    navBtn.classList.toggle('is-active')
-    navMobile.classList.toggle('nav-mobile--active')
+    navBtn.classList.toggle('is-active');
+    navMobile.classList.toggle('nav-mobile--active');
+    allNav.forEach((item) => {
+        item.addEventListener("click", () => {
+          nav.classList.remove("nav--active");
+          allNavItems.forEach((item) => {
+            item.classList.remove("nav-items-animation");
+          });
+        });
+      });
+
 
 }
 
